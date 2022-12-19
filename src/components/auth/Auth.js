865949f7d30558610a0ec996c967f6199198ac2d
CodeSevenic <sevenicbookings@gmail.com>
2022-12-19 10:22:47 +0200

    Tray Solution Code Update

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,55 +1,60 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 export const auth = {
-    isAuthenticated: false,
+  isAuthenticated: false,
 
-    authenticate(cb) {
-        this.isAuthenticated = true
-        if (typeof cb === 'function') {
-            cb(true);
-        }
-    },
-
-    signout(cb) {
-        fetch('/api/logout', {
-            method: 'POST',
-            credentials: 'include',
-        })
-            .then((res) => {
-                this.isAuthenticated = false;
-                if (typeof cb === 'function') {
-                    // user was logged out
-                    cb(true);
-                }
-            })
-            .catch((err) => {
-                console.log('Error logging out user.');
-                if (typeof cb === 'function') {
-                    // user was not logged out
-                    cb(false);
-                }
-            });
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    if (typeof cb === 'function') {
+      cb(true);
     }
+  },
+
+  signout(cb) {
+    fetch('/api/logout', {
+      method: 'POST',
+      credentials: 'include',
+    })
+      .then((res) => {
+        this.isAuthenticated = false;
+        if (typeof cb === 'function') {
+          // user was logged out
+          cb(true);
+        }
+      })
+      .catch((err) => {
+        console.log('Error logging out user.');
+        if (typeof cb === 'function') {
+          // user was not logged out
+          cb(false);
+        }
+      });
+  },
 };
 
-export const PrivateRoute = ({component: Component, ...rest}) => (
-    <Route {...rest} render={props => (
-        auth.isAuthenticated ? (
-            <Component {...props}/>
-        ) : (
-            <Redirect to={{
-                pathname: '/login',
-                state: {from: props.location}
-            }}/>
-        )
-    )}/>
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      auth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
 );
 
-export const RedirectMain = (props) => (
-    auth.isAuthenticated ? (
-        <Redirect {...props} to="/solutions/discover"/>
-    ) : (
-        <Redirect {...props} to="/login"/>
-    )
-);
+export const RedirectMain = (props) =>
+  auth.isAuthenticated ? (
+    <Redirect {...props} to="/solutions/discover" />
+  ) : (
+    <Redirect {...props} to="/login" />
+  );
