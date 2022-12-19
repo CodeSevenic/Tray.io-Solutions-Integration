5062ea1546ba01fe06782c7000aa6b24ae57d4ae
CodeSevenic <sevenicbookings@gmail.com>
2022-12-19 10:00:45 +0200

    Tray Solution Code Update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import App from './Router';
 // import registerServiceWorker from './registerServiceWorker';
 import blue from '@material-ui/core/colors/blue';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 const theme = createMuiTheme({
   palette: {
@@ -15,13 +19,15 @@ const theme = createMuiTheme({
 });
 
 ReactDOM.render(
-  <div className="index-wrapper">
-    <BrowserRouter>
-      <MuiThemeProvider theme={theme}>
-        <App />
-      </MuiThemeProvider>
-    </BrowserRouter>
-  </div>,
+  <Provider store={store}>
+    <div className="index-wrapper">
+      <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    </div>
+  </Provider>,
   document.getElementById('root')
 );
 
