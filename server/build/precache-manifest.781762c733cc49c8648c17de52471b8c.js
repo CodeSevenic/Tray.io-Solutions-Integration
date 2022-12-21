1663e269276a907b0a9bcf43d949bd5705872d4a
CodeSevenic <sevenicbookings@gmail.com>
2022-12-21 10:09:23 +0200

    Tray Solution Code Update

---
+++ server/build/precache-manifest.781762c733cc49c8648c17de52471b8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c78f43e494587b08fdceda51bf5ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c90ca5a39ff39af30362",
+    "url": "/static/css/main.e07b0cbb.chunk.css"
+  },
+  {
+    "revision": "fc13b4975aad4d29d4e3",
+    "url": "/static/js/2.fa607eb4.chunk.js"
+  },
+  {
+    "revision": "c90ca5a39ff39af30362",
+    "url": "/static/js/main.333e7463.chunk.js"
+  },
+  {
+    "revision": "41310a8d7a33ac169b94",
+    "url": "/static/js/runtime-main.d285555b.js"
+  }
+]);
